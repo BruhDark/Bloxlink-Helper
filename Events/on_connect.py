d4a71393d50c705b9033c0e82805e4170e219a42
Dark <fedefunes90@gmail.com>
2022-02-27 15:57:15 -0300

    remove refresh button

--- Events/on_connect.py
+++ Events/on_connect.py
@@ -73,11 +73,8 @@ class OnConnect(commands.Cog):
         
         embed.add_field(name=":snake: PyCord Version", value=f"{pycordV}", inline=True)
         embed.add_field(name="🚄 Railway Status", value=srailway, inline=True)
-   
-        view = discord.ui.View()
-        view.add_item(Button())
 
-        await ctx.respond(embed=embed, view=view)
+        await ctx.respond(embed=embed)
 
     
     @commands.Cog.listener()
@@ -95,73 +92,6 @@ class OnConnect(commands.Cog):
              global started
              started = time.time()
              loaded = True
-
-class Button(discord.ui.Button):
-    def __init__(self):
-        super().__init__(
-            label="Refresh",
-            emoji="🔃",
-            style=discord.enums.ButtonStyle.blurple,
-            disabled = False,
-        )
-    
-    async def callback(self, interaction: discord.Interaction):  
-        
-        seconds = math.floor(time.time() - started)
-
-        m, s = divmod(seconds, 60)
-        h, m = divmod(m, 60)
-        d, h = divmod(h, 24)
-
-        days, hours, minutes, seconds = None, None, None, None
-
-        if d:
-            days = f"{d} Days"
-        if h:
-            hours = f"{h} Hours"
-        if m:
-            minutes = f"{m} Minutes"
-        if s:
-            seconds = f"{s} Seconds"
-
-        uptime = f"{days or ''} {hours or ''} {minutes or ''} {seconds or ''}".strip()
-
-        request = urllib.request.urlopen("https://railway.instatus.com/summary.json")
-        response = json.load(request)
-
-        s = response["page"]
-        srailway = s["status"]
-
-        if srailway == "UP":
-            srailway =  EMOTES["operational"]
-        elif srailway == "HASISSUES":
-            srailway = EMOTES["partialoutage"]
-        elif srailway == "UNDERMAINTENANCE":
-            srailway = EMOTES["maintenance"]
-        else:
-            srailway = EMOTES["question"]
-
-        pycordV = discord.__version__
-
-        embed1 = discord.Embed(description=DESCRIPTION, timestamp=datetime.datetime.utcnow(), color=color)
-
-        embed1.set_author(name=f"{username}", icon_url=icon)
-
-        embed1.add_field(name=f"Bot Version", value="?", inline=True)
-        embed1.add_field(name=":clock1: Uptime", value=uptime, inline=True)
-        latency = str(round(self.bot.latency * 1000))
-        embed1.add_field(name=":ping_pong: Bot Latency", value=f"`{latency}ms`")
-        
-        embed1.add_field(name=":snake: PyCord Version", value=f"{pycordV}", inline=True)
-        embed1.add_field(name="🚄 Railway Status", value=srailway, inline=True)
-   
-        await interaction.message.edit(embed=embed1)
-    
-    async def on_timeout(self, interaction: discord.Interaction):
-        self.disabled = True
-        view = discord.ui.View()
-        view.add_item(self)
-        await interaction.message.edit(view=view)
 
 def setup(bot):
     bot.add_cog(OnConnect(bot))
