ba202f65c31c1ca111fa0b94348d7bc571a0fe3f
Dark <fedefunes90@gmail.com>
2022-02-28 18:27:44 -0300

    Changes to events

--- Events/on_ready.py
+++ Events/on_ready.py
@@ -8,12 +8,6 @@ class OnReady(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
 
-     loaded = False
-
-     if not loaded:
-         await self.bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="the support channels"))
-         loaded = True
-
      print(f"Ready. Logged in as {self.bot.user} - ID: {self.bot.user.id}")
      print("---------")
 
