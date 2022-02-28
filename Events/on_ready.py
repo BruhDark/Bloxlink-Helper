9afb1e3271dac290e4f51230a47a7b466a5985a4
Dark <fedefunes90@gmail.com>
2022-02-28 18:23:18 -0300

    Fix status

--- Events/on_ready.py
+++ Events/on_ready.py
@@ -4,14 +4,15 @@ import discord
 class OnReady(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.loaded = False
 
     @commands.Cog.listener()
     async def on_ready(self):
 
-     if not self.loaded:
+     loaded = False
+
+     if not loaded:
          await self.bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="the support channels"))
-         self.loaded = True
+         loaded = True
 
      print(f"Ready. Logged in as {self.bot.user} - ID: {self.bot.user.id}")
      print("---------")
