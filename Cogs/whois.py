73bd28d22658a72a985b54255c5635cbab0762e4
Dark <fedefunes90@gmail.com>
2022-04-04 09:10:00 -0300

    Xdnt it was user.display_name

--- Cogs/whois.py
+++ Cogs/whois.py
@@ -63,7 +63,7 @@ class Whois(commands.Cog):
             color = COLORS["info"]
 
         amember = "**This user is not a member of this server.**" if noMember else ""
-        nickname = user.nickname if not noMember else ""
+        nickname = user.display_name if not noMember else ""
 
         Embed = discord.Embed(color=color, timestamp=datetime.datetime.utcnow(
         ), description=f"{user.mention}\n**Nickname** - {nickname}\n{amember}")
