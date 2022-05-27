6f37dfa69c85c8636fcb8c3fb06cb348472bc775
Dark <fedefunes90@gmail.com>
2022-05-27 11:06:01 -0300

    Create custom paginator to override view checks

---
+++ Resources/paginator.py
@@ -0,0 +1,19 @@
+from discord.ext.pages import Paginator
+import discord
+from config import EMOTES, COLORS
+
+e = EMOTES["error"]
+ce = COLORS["error"]
+embed = discord.Embed(description=f"{e} This paginator is not for you!", color=ce)
+
+class CustomPaginator(Paginator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if self.usercheck:
+            if self.user != interaction.user:
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+                return False
+            return self.user == interaction.user
+        return True
