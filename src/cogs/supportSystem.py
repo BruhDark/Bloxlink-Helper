5af11f3ae53a1c68449ae0417b61f354717e1f4a
Dark <fedefunes90@gmail.com>
2022-07-22 19:38:40 -0300

    Fix?

--- src/cogs/supportSystem.py
+++ src/cogs/supportSystem.py
@@ -40,10 +40,9 @@ class SupportSystem(commands.Cog):
         modal = FAQEditModal(category)
         await ctx.send_modal(modal)
 
-    @faq.command()
-    @is_staff()
+    @discord.slash_command()
     @commands.is_owner()
-    async def send(self, ctx, channel: discord.TextChannel):
+    async def send_faq(self, ctx, channel: discord.TextChannel):
         """Owner only command to send the FAQs to a channel."""
 
         embed = discord.Embed(color=COLORS["info"], title=":wave: Welcome to Bloxlink's support system!", description="Welcome! You have come to the right place if you are looking for help with Bloxlink.\n\n<:lifesaver:986648046592983150> **How to use the support system**\n\nIt is very easy to use our support system! You can navigate through a quick Frequently Asked Questions menu and get a fast answer by clicking the **Open FAQ** button. If your question is not in any of the categories, you can always open a support thread by clicking the **Get Support** button below the FAQs. Easy, isn't it?")
@@ -51,10 +50,9 @@ class SupportSystem(commands.Cog):
         await channel.send(embed=embed, view=SupportView())
         await ctx.respond("Done")
 
-    @faq.command()
-    @is_staff()
+    @discord.slash_command()
     @commands.is_owner()
-    async def update(self, ctx: discord.ApplicationContext, channel: discord.TextChannel, message: str):
+    async def update_faq(self, ctx: discord.ApplicationContext, channel: discord.TextChannel, message: str):
         """Owner only command to update the support system message."""
 
         message = await channel.fetch_message(int(message))
@@ -64,8 +62,7 @@ class SupportSystem(commands.Cog):
         await message.edit(embed=embed, view=SupportView())
         await ctx.respond("Done")
 
-    @faq.command()
-    @is_staff()
+    @discord.slash_command()
     @commands.is_owner()
     async def send_get_support(self, ctx: discord.ApplicationContext, channel: discord.TextChannel):
         """Owner only command to send the get support message."""
