2942ce408e3f6ced509087f16af62c2d38fd64a1
Dark <fedefunes90@gmail.com>
2022-08-04 21:50:04 -0300

    Switch to classes

--- src/config.py
+++ src/config.py
@@ -3,67 +3,78 @@ import discord
 PREFIX = "."
 DESCRIPTION = "Bloxlink HQ's tags bot."
 
-EMOTES = {
-    "success": "<:BloxlinkHappy:823633735446167552>",
-    "error": "<:BloxlinkDead:823633973967716363>",
-    "success2": "<a:BHDone:976817167116947507>",
-
-    "warning": ":warning:",
-    "info": "<:info:881973831974154250>",
-    "question": "<:Question:929051955962195999>",
-
-    "operational": "<:Operational:882404710148083724>",
-    "partialoutage": "<:PartialOutage:882404755949895730>",
-    "majoroutage": "<:MajorOutage:882404641286000681>",
-    "maintenance": "<:UnderMaintenance:881969909247148052>",
-
-    "dnd": "<:DoNotDisturb:928716110822518834>",
-    "idle": "<:Idle:929049988573581352>",
-    "online": "<:Online:928716318054694993>",
-    "offline": "<:Offline:928716879474851870>",
-
-    "loading": "<a:loading:946809583194767400>",
-    "spotify": "<:Spotify:929060525881565224>",
-    "943150072827355177": "<:BloxlinkSilly:823634273604468787>",
-
-    "add": "<:add:930273857363927110>",
-    "remove": "<:edit:930273882919821322>",
-    "edit": "<:remove:930273905569042472>",
-}
-
-LINKS = {
-    "success": "https://cdn.discordapp.com/emojis/976817167116947507.gif?size=44&quality=lossless",
-    "error": "https://cdn.discordapp.com/emojis/823633973967716363.webp?size=96&quality=lossless",
-    "other": "https://cdn.discordapp.com/emojis/823634273604468787.webp?size=96&quality=lossless",
-    "heart": "https://media.discordapp.net/attachments/947298060646613032/996566385750712400/BloxHeart.png"
-}
-
-COLORS = {
-    "success": discord.Colour.from_rgb(67, 181, 130),
-    "error": discord.Colour.from_rgb(240, 74, 71),
-    "warning": discord.Colour.from_rgb(255, 155, 0),
-    "info": discord.Colour.from_rgb(82, 113, 255),
-}
-
-RELEASESCOLORS = {
-    "943150072827355177": discord.Colour.from_rgb(82, 113, 255),
-    "912846359789461525": discord.Colour.from_rgb(82, 113, 255)
-}
-
 AUTHORIZED = [449245847767482379, 431480956990390272, 156872400145874944]
 
-BADGES = {
-    "bot": "<:Bot:928765691778203719>",
-    "bughunter": "<:BugHunter:928765827107422268>",
-    "moderator": "<:DiscordCertifiedModerator:881971670921916518>",
-    "staff": "<:DiscordStaff:882405445136965633>",
-    "events": "<:HypeSquadEvents:928765434281488534>",
-    "balance": "<:HypeSquad_Balance:928765104110047273>",
-    "bravery": "<:HypeSquad_Bravery:928765052427841628>",
-    "brilliance": "<:HypeSquad_Brilliance:928764974103404554>",
-    "verifiedbot": "<:VerifiedBot:848277286927073280>",
-    "bughunter2": "<:BugHunter:928768063933939783>",
-    "partner": "<:DiscordPartner:928768159215923230>",
-    "early": "<:EarlySupporter:928768119072231454>",
-    "botdev": "<:VerfiedBotDeveloper:928765568058785852>"
-}
+
+class Emotes:
+    def __init__(self):
+        self.success = "<:BloxlinkHappy:823633735446167552>"
+        self.error: str = "<:BloxlinkDead:823633973967716363>"
+        self.success2: str = "<a:BHDone:976817167116947507>"
+        self.warning = ":warning:"
+        self.info = "<:info:881973831974154250>"
+        self.question = "<:Question:929051955962195999>"
+        self.operational = "<:Operational:882404710148083724>",
+        self.partialoutage = "<:PartialOutage:882404755949895730>",
+        self.majoroutage = "<:MajorOutage:882404641286000681>",
+        self.maintenance = "<:UnderMaintenance:881969909247148052>",
+
+        self.dnd = "<:DoNotDisturb:928716110822518834>",
+        self.idle = "<:Idle:929049988573581352>",
+        self.online = "<:Online:928716318054694993>",
+        self.offline = "<:Offline:928716879474851870>",
+
+        self.loading = "<a:loading:946809583194767400>",
+        self.spotify = "<:Spotify:929060525881565224>",
+        self.bloxlink = "<:BloxlinkSilly:823634273604468787>",
+
+        self.add = "<:add:930273857363927110>",
+        self.remove = "<:edit:930273882919821322>",
+        self.edit = "<:remove:930273905569042472>",
+
+
+class Links:
+    def __init__(self):
+        self.success = "https://cdn.discordapp.com/emojis/976817167116947507.gif?size=44&quality=lossless",
+        self.error = "https://cdn.discordapp.com/emojis/823633973967716363.webp?size=96&quality=lossless",
+        self.other = "https://cdn.discordapp.com/emojis/823634273604468787.webp?size=96&quality=lossless",
+        self.heart = "https://media.discordapp.net/attachments/947298060646613032/996566385750712400/BloxHeart.png"
+
+
+class Colors:
+    def __init__(self):
+        self.success = discord.Colour.from_rgb(67, 181, 130)
+        self.error = discord.Colour.from_rgb(240, 74, 71)
+        self.warning = discord.Colour.from_rgb(255, 155, 0)
+        self.info = discord.Colour.from_rgb(82, 113, 255)
+        self.main = discord.Colour.from_rgb(82, 113, 255)
+
+
+class Releasescolors:
+    def __init__(self):
+        self.main = discord.Colour.from_rgb(82, 113, 255)
+        self.local = discord.Colour.from_rgb(82, 113, 255)
+
+
+class Badges:
+    def __init__(self):
+        self.bot = "<:Bot:928765691778203719>",
+        self.bughunter = "<:BugHunter:928765827107422268>",
+        self.moderator = "<:DiscordCertifiedModerator:881971670921916518>",
+        self.staff = "<:DiscordStaff:882405445136965633>",
+        self.events = "<:HypeSquadEvents:928765434281488534>",
+        self.balance = "<:HypeSquad_Balance:928765104110047273>",
+        self.bravery = "<:HypeSquad_Bravery:928765052427841628>",
+        self.brilliance = "<:HypeSquad_Brilliance:928764974103404554>",
+        self.verfiedbot = "<:VerifiedBot:848277286927073280>",
+        self.bughunter2 = "<:BugHunter:928768063933939783>",
+        self.partner = "<:DiscordPartner:928768159215923230>",
+        self.early = "<:EarlySupporter:928768119072231454>",
+        self.botdev = "<:VerfiedBotDeveloper:928765568058785852>"
+
+
+colors = Colors()
+links = Links()
+emotes = Emotes()
+badges = Badges()
+releasescolors = Releasescolors()
