74417fa3a4178510925cac082d7f47d740cb3ab9
Dark <fedefunes90@gmail.com>
2022-08-11 21:23:10 -0300

    Add reverse lookup, verified check

--- src/cogs/api.py
+++ src/cogs/api.py
@@ -11,25 +11,63 @@ class ApiCommand(commands.Cog):
 
     @commands.command()
     @commands.guild_only()
-    async def api(self, ctx: discord.ApplicationContext, query: str):
+    async def api(self, ctx: discord.ApplicationContext, *, query: str):
+        verified = discord.utils.get(ctx.guild.roles, name="Verified")
+        if verified not in ctx.author.roles:
+            embed = discord.Embed(
+                description=f"{emotes.error} You must be verified with Bloxlink to use this command", color=colors.error)
+            await ctx.send(embed=embed)
+            return
 
-        url = f"https://v3.blox.link/developer/discord/{query}"
         headers = {
             "api-key": "657809d4-8daa-4658-bf83-0084d643c88b2b10def8-b27e-4713-a17b-02e5e562aa100c8ee099-a366-4e71-bbdb-946f826646d8"}
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=headers) as response:
-                data = await response.text()
-                headers = response.headers
-                quota = headers["quota-remaining"]
+        query = query.lower()
 
-                embed = discord.Embed(timestamp=datetime.datetime.utcnow(
-                ), description=f"Sent request to {url}.\n\n**Response**\n```json\n{data}```", color=colors.info)
-                embed.set_author(icon_url=links.success,
-                                 name="Successfully sent request")
-                embed.set_footer(text=f"Quota remaining: {quota} requests")
+        if query.startswith("reverse"):
+            query = query.split(" ")
+            print(query)
+            query = query[1]
+            url = f"https://v3.blox.link/developer/roblox/{query}"
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, headers=headers) as resp:
+                    data = await resp.json()
+                    headers = resp.headers
+                    try:
+                        quota = headers["quota-remaining"]
+                    except:
+                        quota = "?"
 
-                await ctx.send(embed=embed)
+                    embed = discord.Embed(timestamp=datetime.datetime.utcnow(
+                    ), description=f"Sent request to {url}.\n\n**Response**\n```json\n{data}```", color=colors.info)
+                    embed.set_author(icon_url=links.success,
+                                     name="Successfully sent request")
+                    embed.set_footer(text=f"{quota} requests remaining")
+
+                    await ctx.send(embed=embed)
+
+        else:
+
+            url = f"https://v3.blox.link/developer/discord/{query}"
+            headers = {
+                "api-key": "657809d4-8daa-4658-bf83-0084d643c88b2b10def8-b27e-4713-a17b-02e5e562aa100c8ee099-a366-4e71-bbdb-946f826646d8"}
+
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, headers=headers) as response:
+                    data = await response.text()
+                    headers = response.headers
+                    try:
+                        quota = headers["quota-remaining"]
+                    except:
+                        quota = "?"
+
+                    embed = discord.Embed(timestamp=datetime.datetime.utcnow(
+                    ), description=f"Sent request to {url}.\n\n**Response**\n```json\n{data}```", color=colors.info)
+                    embed.set_author(icon_url=links.success,
+                                     name="Successfully sent request")
+                    embed.set_footer(text=f"{quota} requests remaining")
+
+                    await ctx.send(embed=embed)
 
 
 def setup(bot):
