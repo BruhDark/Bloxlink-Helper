37a065d7bd5d52a1c4481371a9015b16f4051a2a
Dark <fedefunes90@gmail.com>
2023-06-14 20:51:52 -0300

    Add commandless tags

--- src/events/on_command_error.py
+++ src/events/on_command_error.py
@@ -1,6 +1,6 @@
 import datetime
 import os
-import sys
+import asyncio
 import traceback
 from types import NoneType
 
@@ -10,11 +10,51 @@ from config import colors, emotes
 from discord.ext import commands
 
 from resources.context import CommandsContext
+from resources.mongoFunctions import find_tag
 
 
 class OnCmdError(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    async def parse_possible_tag(self, ctx: commands.Context, message: str):
+
+        text = message.split()
+        p_tag = text[0].replace(".", "")
+        if p_tag == "":
+            return
+
+        tag = await find_tag(p_tag)
+        text = " ".join(text[1:])
+        text = text if text != "" else None
+
+        message = ctx.message.reference.message_id if ctx.message.reference is not None else None
+        message = discord.utils.get(
+            self.bot.cached_messages, id=message) if message is not None else None
+
+        if tag:
+            tag_content = tag["content"]
+            if text is not None:
+                await ctx.message.delete()
+
+                if message is not None:
+                    msg = await message.reply(content=tag_content, mention_author=True)
+                    await asyncio.sleep(0.1)
+                    await msg.edit(f"{text} {tag_content}")
+
+                else:
+                    msg = await ctx.send(f"{tag_content}")
+                    await asyncio.sleep(0.1)
+                    await msg.edit(f"{text} {tag_content}")
+
+            elif message is not None:
+
+                await ctx.message.delete()
+                await message.reply(f"{tag_content}")
+
+            else:
+                await ctx.message.delete()
+                await ctx.send(f"{tag_content}")
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx: CommandsContext, error: Exception):
@@ -33,7 +73,7 @@ class OnCmdError(commands.Cog):
             await ctx.error(f"You are missing a required argument: `{error.param.name}`")
 
         elif isinstance(error, commands.CommandNotFound):
-            pass
+            await self.parse_possible_tag(ctx, ctx.message.content)
 
         elif isinstance(error, commands.NoPrivateMessage):
             await ctx.error("This command is only available in a guild!")
