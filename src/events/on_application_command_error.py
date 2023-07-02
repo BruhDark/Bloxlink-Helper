919f0d8348e61475f1658117621fd6be25815bec
Dark <fedefunes90@gmail.com>
2023-07-02 12:36:11 -0300

    Use webhook manager

--- src/events/on_application_command_error.py
+++ src/events/on_application_command_error.py
@@ -1,14 +1,7 @@
-from datetime import datetime
-import os
-import sys
-import traceback
-
-import aiohttp
-import discord
-from config import colors, emotes
 from discord.ext import commands
 
 from resources.context import ApplicationCommandsContext
+from resources import webhook_manager
 
 
 class OnApplicationCmdError(commands.Cog):
@@ -29,24 +22,8 @@ class OnApplicationCmdError(commands.Cog):
 
         else:
 
+            await webhook_manager.send_command_error(ctx, error)
             await ctx.error(f"Something went wrong\n\n```py\n{error}```")
-
-            async with aiohttp.ClientSession() as session:
-                url = os.getenv("WEBHOOK_URL")
-                webhook = discord.Webhook.from_url(url, session=session)
-                tb = ''.join(traceback.format_exception(
-                    error, error, error.__traceback__))
-                tb = tb + "\n"
-
-                embed = discord.Embed(
-                    title=f"{emotes.error} Something Went Wrong", color=colors.error, timestamp=datetime.utcnow())
-                embed.description = f"```py\n{tb}```"
-                embed.add_field(
-                    name="Command", value=f"{ctx.command.qualified_name}")
-                embed.add_field(
-                    name="Guild", value=f"{ctx.guild.name} ({ctx.guild.id})")
-
-                await webhook.send(embed=embed)
 
 
 def setup(bot):
--- src/events/on_command_error.py
+++ src/events/on_command_error.py
@@ -1,16 +1,10 @@
-import datetime
-import os
 import asyncio
-import traceback
-from types import NoneType
-
-import aiohttp
 import discord
-from config import colors, emotes
 from discord.ext import commands
 
 from resources.context import CommandsContext
 from resources.mongoFunctions import find_tag
+from resources import webhook_manager
 
 
 class OnCmdError(commands.Cog):
@@ -85,24 +79,8 @@ class OnCmdError(commands.Cog):
             await ctx.error("This command is disabled!")
 
         else:
+            await webhook_manager.send_command_error(ctx, error)
             await ctx.error(f"Something went wrong\n\n```py\n{tb}```")
-
-            async with aiohttp.ClientSession() as session:
-                url = os.getenv("WEBHOOK_URL")
-                webhook = discord.Webhook.from_url(url, session=session)
-                tb = ''.join(traceback.format_exception(
-                    error, error, error.__traceback__))
-                tb = tb + "\n"
-
-                embed = discord.Embed(
-                    title=f"{emotes.error} Something Went Wrong", color=colors.error, timestamp=datetime.datetime.utcnow())
-                embed.description = f"```py\n{tb}```"
-                embed.add_field(
-                    name="Command", value=f"{ctx.command.qualified_name}")
-                embed.add_field(
-                    name="Guild", value=f"{ctx.guild.name} ({ctx.guild.id})")
-
-                await webhook.send(embed=embed)
 
 
 def setup(bot):
